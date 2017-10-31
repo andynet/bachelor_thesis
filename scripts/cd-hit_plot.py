2f89ffebc12c414189aacf5c8a66440d3c159ca8
andy <andrejbalaz001@gmail.com>
2017-10-31 10:43:26 +0100

    cd-hit_plot changed to calculate number of reads after cd-hit

--- scripts/cd-hit_plot.py
+++ scripts/cd-hit_plot.py
@@ -9,36 +9,14 @@ if len(sys.argv) != 2:
     print('Usage:', sys.argv[0], '<fasta>')
     exit()
 
-
-def get_matrix_density_from_clstr(clstr):
-
-    highest_clstr_num = 0
-    density = 0
-
-    for line in clstr:
-
-        if line[0] == '>':
-            density += highest_clstr_num * highest_clstr_num
-            highest_clstr_num = 0
-        else:
-            clstr_num = int(line.split('\t')[0])
-            highest_clstr_num = max(highest_clstr_num, clstr_num+1)
-
-    return density
-
-
-densities = []
+counts = []
 percentages = []
 fasta = sys.argv[1]
-
-# with open(fasta) as f:
-#     lines = f.readlines()
-#     matrix_size = len(lines/2) * len(lines/2)
 
 for i in range(100, 50, -1):
 
     perc = i/100
-    command = 'cd-hit -c {} -d 0 -g 1 -i {} -o {}_{} -T 10'.format(perc, fasta, os.path.basename(fasta), perc)
+    command = 'cd-hit -c {} -d 0 -g 1 -i {} -o {}_{}.fa -T 10'.format(perc, fasta, os.path.basename(fasta), perc)
 
     try:
         print(command)
@@ -47,14 +25,16 @@ for i in range(100, 50, -1):
         print('Finished at', perc, '%')
         break
 
-    with open('{}_{}.clstr'.format(os.path.basename(fasta), perc)) as f:
+    with open('{}_{}.fa'.format(os.path.basename(fasta), perc)) as f:
         lines = f.readlines()
-        density = get_matrix_density_from_clstr(lines)
-        densities.append(density)
+        count = len(lines)/2
+        print(count)
+
+        counts.append(count)
         percentages.append(perc)
 
 plt.figure()
-plt.plot(percentages, densities)
+plt.plot(percentages, counts)
 plt.show()
 plt.savefig(os.path.basename(fasta) + '.figure.png')
 
