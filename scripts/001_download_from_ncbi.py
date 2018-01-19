53090b00e12fccd39ac09b822591f5a84f4f00a5
andy <andrejbalaz001@gmail.com>
2018-01-19 09:52:35 +0100

    exception solved

--- scripts/001_download_from_ncbi.py
+++ scripts/001_download_from_ncbi.py
@@ -2,6 +2,7 @@
 
 from http.client import IncompleteRead
 from Bio.SeqRecord import SeqRecord
+from urllib.error import HTTPError
 from Bio import Entrez
 from Bio import SeqIO
 import time
@@ -11,7 +12,8 @@ import sys
 def safe_get_qualifier(feature, key, default_value='NO_DATA'):
     try:
         return ';'.join(feature.qualifiers[key])
-    except KeyError:
+    except KeyError as e:
+        # print(e)
         return default_value
 
 
@@ -53,25 +55,34 @@ while i < len(record['IdList']):
     fa_handle = None
     gb_handle = None
 
-    while fa_handle is None or gb_handle is None:
+    try:
         fa_handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", idtype="acc",
                                   retstart=i, retmax=batch, webenv=webenv, query_key=query_key)
         gb_handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text",
                                   retstart=i, retmax=batch, webenv=webenv, query_key=query_key)
+    except HTTPError as e:
+        print(e)
+        time.sleep(10)
+        if fa_handle is not None:
+            fa_handle.close()
+        if gb_handle is not None:
+            gb_handle.close()
+        continue
 
     try:
         fa_list = list(SeqIO.parse(fa_handle, 'fasta'))
         gb_list = list(SeqIO.parse(gb_handle, 'genbank'))
-    except IncompleteRead:
-        print('Incomplete read. Trying again...')
+    except IncompleteRead as e:
+        print(e)
         time.sleep(10)
         continue
     except ValueError as e:
         print(e)
+        time.sleep(10)
         continue
-
-    fa_handle.close()
-    gb_handle.close()
+    finally:
+        fa_handle.close()
+        gb_handle.close()
 
     for j in range(0, len(fa_list)):
 
