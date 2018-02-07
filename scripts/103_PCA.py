a5fa519e34912bad36438c8f0c6fcb5d9d8afbb7
andy <andrejbalaz001@gmail.com>
2018-02-07 17:32:00 +0100

    PCA done

--- scripts/103_PCA.py
+++ scripts/103_PCA.py
@@ -1,55 +1,76 @@
 #!/usr/bin/python3
 
-# TODO:
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+from matplotlib.colors import Normalize
 import pandas as pd
 import sys
 import os
 
 
+def get_hosts(file):
+
+    with open(file) as f:
+        lines = f.readlines()
+
+    hosts = dict()
+    for line in lines:
+        phage = line.split()[0]
+        host = line.split()[1]
+
+        hosts[phage] = host
+
+    return hosts
+
+
 def on_pick(event):
 
-    global labels
+    global matrices
+    global groups
+    global hosts
 
-    print('you pressed', event.ind)
+    print('--------------------------------------------------------------------------------')
+    gind = groups.index(event.artist)
 
-    for ind in event.ind:
-        print(labels[ind])
+    for eind in event.ind:
+        phage = matrices[gind].index[eind]
+        print('{}\t{}'.format(phage, hosts[phage]))
 
 
-if len(sys.argv) < 5 or len(sys.argv) % 2 != 1:
-    print('Usage:', sys.argv[0], '<label> <matrix> <label> <matrix>...')
+if len(sys.argv) < 4 or len(sys.argv) % 2 != 0:
+    print('Usage:', sys.argv[0], '<hosts> <label> <matrix> <label> <matrix>...')
     exit()
 
 data_dir = os.path.dirname(os.path.abspath(sys.argv[1]))
 
-labels = []
+labels = [0]
 matrices = []
 
-for i in range(len(sys.argv)//2):
-    with open(sys.argv[i*2+1]) as f:
-        labels += [i]*len(f.readlines())
+for i in range(0, len(sys.argv)//2 - 1):
+    with open(sys.argv[i*2+2]) as f:
+        labels.append(labels[-1]+len(f.readlines()))
 
-    matrices.append(pd.read_csv(sys.argv[i*2+2], sep='\t', header=0, index_col=0))
+    matrices.append(pd.read_csv(sys.argv[i*2+3], sep='\t', header=0, index_col=0))
 
-# print('Labels:', labels)
-# print('Matrices:', matrices)
-
-# merge matrices
 features = pd.concat(matrices)
-
-color_map = mpl.colors.ListedColormap([[0.5, 0.5, 0.5], [0.95, 0.95, 0.95], [1.0, 0.0, 0.0]])
+hosts = get_hosts(sys.argv[1])
 
 fig = plt.figure()
-X_reduced = PCA(18).fit_transform(features)
-
-pca = PCA(18)
+pca = PCA(10)
 pca.fit(features)
 print(pca.explained_variance_ratio_)
+pca = pca.transform(features)
 
-plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=labels, cmap=color_map, edgecolor='k', s=100, picker=True)
+cmap = plt.get_cmap('jet')
+norm = Normalize(vmin=0, vmax=len(labels))
+
+groups = []
+for i in range(1, len(labels)):
+    group = plt.scatter(pca[labels[i-1]:labels[i], 0], pca[labels[i-1]:labels[i], 1],
+                        c=[cmap(norm(i))]*(labels[i]-labels[i-1]), edgecolor='k', picker=True,
+                        label=sys.argv[i*2])
+    groups.append(group)
+
 cid = fig.canvas.mpl_connect('pick_event', on_pick)
-
+plt.legend()
 plt.show()
