73d9079ce7dce1d93a1db44f8e397a335840c814
andy <andrejbalaz001@gmail.com>
2018-02-12 16:55:25 +0100

    created PCA graphs

--- scripts/103_PCA.py
+++ scripts/103_PCA.py
@@ -45,32 +45,56 @@ data_dir = os.path.dirname(os.path.abspath(sys.argv[1]))
 
 labels = [0]
 matrices = []
+number_of_records = []
 
 for i in range(0, len(sys.argv)//2 - 1):
     with open(sys.argv[i*2+2]) as f:
         labels.append(labels[-1]+len(f.readlines()))
 
-    matrices.append(pd.read_csv(sys.argv[i*2+3], sep='\t', header=0, index_col=0))
+    matrix = pd.read_csv(sys.argv[i*2+3], sep='\t', header=0, index_col=0)
+    matrices.append(matrix)
 
 features = pd.concat(matrices)
 hosts = get_hosts(sys.argv[1])
 
-fig = plt.figure()
-pca = PCA(10)
-pca.fit(features)
-print(pca.explained_variance_ratio_)
-pca = pca.transform(features)
+pca_comps = PCA(8)
+pca_comps.fit(features)
+print(pca_comps.explained_variance_ratio_)
+pca = pca_comps.transform(features)
 
 cmap = plt.get_cmap('jet')
 norm = Normalize(vmin=0, vmax=len(labels))
 
+fig = plt.figure()
+
 groups = []
 for i in range(1, len(labels)):
-    group = plt.scatter(pca[labels[i-1]:labels[i], 0], pca[labels[i-1]:labels[i], 1],
-                        c=[cmap(norm(i))]*(labels[i]-labels[i-1]), edgecolor='k', picker=True,
+    group = plt.scatter(pca[labels[i-1]:labels[i], 5], pca[labels[i-1]:labels[i], 7],
+                        c=[cmap(norm(i))]*(labels[i]-labels[i-1]), edgecolor='k', s=50, picker=True, alpha=0.3,
                         label=sys.argv[i*2])
     groups.append(group)
 
 cid = fig.canvas.mpl_connect('pick_event', on_pick)
-plt.legend()
+plt.legend(loc='best', fontsize='x-small')
 plt.show()
+
+# generate all graphs
+# for j in range(8):
+#     for k in range(8):
+#
+#         fig = plt.figure()
+#         groups = []
+#
+#         for i in range(1, len(labels)):
+#             group = plt.scatter(pca[labels[i-1]:labels[i], j], pca[labels[i-1]:labels[i], k],
+#                                 c=[cmap(norm(i))]*(labels[i]-labels[i-1]), edgecolor='k', s=50, picker=True,
+#                                 alpha=0.3, label=sys.argv[i*2])
+#             groups.append(group)
+#
+#         plt.legend(loc='best', fontsize='x-small')
+#         fig.savefig('104_PC{}_PC{}.png'.format(j, k), dpi=300)
+#         plt.close(fig)
+
+# plt.figure()
+# plt.plot(pca_comps.components_[0], '.')
+# plt.show()
